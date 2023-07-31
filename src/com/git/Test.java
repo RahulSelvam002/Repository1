7b789591ac253c1c4b1d019e40a78ca97456b0d6
RahulSelvam002 <rahulselvam002@gmail.com>
2023-07-31 13:56:00 +0530

    Code Added

---
+++ src/com/git/Test.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Test {
+	public void task() {
+		System.out.println("Welcome");
+	}
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.task();
+	}
+}
