205c2ed5bf185dfdf29093523ed9c52a97dbee77
RahulSelvam002 <rahulselvam002@gmail.com>
2023-07-31 14:18:08 +0530

    Set added

--- src/com/git/Test.java
+++ src/com/git/Test.java
@@ -1,5 +1,8 @@
 package com.git;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test {
 	public void task() {
 		System.out.println("Welcome");
@@ -7,5 +10,9 @@ public class Test {
 	public static void main(String[] args) {
 		Test t = new Test();
 		t.task();
+		
+		Set<Integer> s = new HashSet<>();
+		s.add(100);
+		s.add(200);
 	}
 }
