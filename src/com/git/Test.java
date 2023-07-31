392af0f1d3e41de90ef0019900bcdb591dbfb73b
RahulSelvam002 <rahulselvam002@gmail.com>
2023-07-31 14:13:57 +0530

    List added

--- src/com/git/Test.java
+++ src/com/git/Test.java
@@ -1,5 +1,8 @@
 package com.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 	public void task() {
 		System.out.println("Welcome");
@@ -7,5 +10,9 @@ public class Test {
 	public static void main(String[] args) {
 		Test t = new Test();
 		t.task();
+		
+		List<String> l = new ArrayList<String>();
+		l.add("100");
+		l.add("200");
 	}
 }
